991202fe2ce1f60d7f0ce8a0d559e19fcbde87a5
David Freerksen <dfreerksen@gmail.com>
2014-08-29 12:55:46 -0700

    Correct an rspec deprecation

--- spec/helpers/spree/admin/carousels_helper_spec.rb
+++ spec/helpers/spree/admin/carousels_helper_spec.rb
@@ -3,43 +3,43 @@ require 'spec_helper'
 describe Spree::Admin::CarouselsHelper do
   context '#carousel_location_values' do
     it 'should return 6 items' do
-      expect(helper.carousel_location_values).to have(6).items
+      expect(helper.carousel_location_values.size).to eq(6)
     end
   end
 
   context '#carousel_easing_values' do
     it 'should return 31 items' do
-      expect(helper.carousel_easing_values).to have(31).items
+      expect(helper.carousel_easing_values.size).to eq(31)
     end
   end
 
   context '#carousel_lazy_load_values' do
     it 'should return 2 items' do
-      expect(helper.carousel_lazy_load_values).to have(2).items
+      expect(helper.carousel_lazy_load_values.size).to eq(2)
     end
   end
 
   context '#carousel_speed_values' do
     it 'should return 77 items' do
-      expect(helper.carousel_speed_values).to have(77).items
+      expect(helper.carousel_speed_values.size).to eq(77)
     end
   end
 
   context '#carousel_autoplay_speed_values' do
     it 'should return 199 items' do
-      expect(helper.carousel_autoplay_speed_values).to have(199).items
+      expect(helper.carousel_autoplay_speed_values.size).to eq(199)
     end
   end
 
   context '#carousel_slides_values' do
     it 'should return 10 items' do
-      expect(helper.carousel_slides_values).to have(10).items
+      expect(helper.carousel_slides_values.size).to eq(10)
     end
   end
 
   context '#carousel_limit_values' do
     it 'should return 20 items' do
-      expect(helper.carousel_limit_values).to have(20).items
+      expect(helper.carousel_limit_values.size).to eq(20)
     end
   end
 end
